7e07e3090e09c6ab1c57f9b6004724c065899acb
Guilherme Santana <g.santossantana22@gmail.com>
2023-01-18 10:43:15 -0300

    recursos do mapview

--- App.js
+++ App.js
@@ -3,19 +3,27 @@ import MapView from "react-native-maps";
 
 export default function App() {
   const regiaoInicial = {
-    latitude: 52.479761,
-    longitude: 62.185661,
+    latitude: 37.401437,
+    longitude: -116.86773,
     latitudeDelta: 0.0922,
     /*  longitudeDelta: 0.0421, */
-    latitudeDelta: 0,
-    longitudeDelta: 0,
+    latitudeDelta: 0.5,
+    longitudeDelta: 0.4,
   };
 
   return (
     <>
       <StatusBar />
       <View style={estilos.container}>
-        <MapView style={estilos.map} initialRegion={regiaoInicial} />
+        <MapView
+          style={estilos.map}
+          initialRegion={regiaoInicial}
+          liteMode={false}
+          mapType="standard" // satellite, hybrid, terrain
+          userInterfaceStyle="dark" //somente IOS
+          maxZoomLevel={5}
+          minZoomLevel={2}
+        />
       </View>
     </>
   );
--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 ## 01-api-mapview
 
 **Documentação:** https://docs.expo.dev/versions/v47.0.0/sdk/map-view/
+https://github.com/react-native-maps/react-native-maps/blob/master/docs/mapview.md
+
+## 02-
 
 ## Instalação
+
+`npx expo install react-native-maps`
