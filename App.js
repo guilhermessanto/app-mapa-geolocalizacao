c6b4474e073d123bd4561370ca908f39038006d7
Guilherme Santana <g.santossantana22@gmail.com>
2023-01-18 10:00:33 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import { StyleSheet, Text, View, StatusBar } from "react-native";
+import MapView from "react-native-maps";
 
 export default function App() {
+  const regiaoInicial = {
+    latitude: 52.479761,
+    longitude: 62.185661,
+    latitudeDelta: 0.0922,
+    /*  longitudeDelta: 0.0421, */
+    latitudeDelta: 0,
+    longitudeDelta: 0,
+  };
+
   return (
     <>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar />
+      <StatusBar />
+      <View style={estilos.container}>
+        <MapView style={estilos.map} initialRegion={regiaoInicial} />
       </View>
     </>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
