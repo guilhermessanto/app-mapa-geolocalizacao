560a827e494a449c11a47e0c5e5f82eb842f62cd
Guilherme Santana <g.santossantana22@gmail.com>
2023-01-19 09:57:30 -0300

    outra forma de corrigir exercicio

--- App.js
+++ App.js
@@ -10,19 +10,11 @@ export default function App() {
     latitudeDelta: 0.0222,
     longitudeDelta: 0.00121,
   };
-  const [localizacao, setLocalizacao] = useState({
-    latitude: 37.401437,
-    longitude: -116.86773,
-    latitudeDelta: 0.0222,
-    longitudeDelta: 0.00121,
-  });
+  const [localizacao, setLocalizacao] = useState();
   const novaLocalizacao = (event) => {
-    let coordenadas = {
+    setLocalizacao({
       latitude: event.nativeEvent.coordinate.latitude,
       longitude: event.nativeEvent.coordinate.longitude,
-    };
-    setLocalizacao({
-      ...coordenadas,
       latitudeDelta: 0.0222,
       longitudeDelta: 0.00121,
     });
@@ -35,12 +27,15 @@ export default function App() {
       <View style={estilos.container}>
         <MapView
           style={estilos.map}
-          initialRegion={regiaoInicial}
+          // initialRegion={regiaoInicial}
+          region={localizacao ?? regiaoInicial}
           liteMode={false}
           mapType="satellite"
           onPress={novaLocalizacao}
         >
-          <Marker coordinate={localizacao} title="Titulo" draggable />
+          {localizacao && (
+            <Marker coordinate={localizacao} title="Titulo" draggable />
+          )}
         </MapView>
       </View>
     </>
